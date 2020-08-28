a7fa51ca755d431ee0e3e4872624e62117547c82
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-08-28 12:06:29 +0300

    fix building on macos

--- wireshark-epan-adapter/build.rs
+++ wireshark-epan-adapter/build.rs
@@ -1,37 +1,38 @@
 use std::{process::Command, str, env, path::PathBuf};
 
 fn main() {
-    // will use well known path of headers,
-    // can be override by env variable `WIRESHARK_EPAN_HEADERS`
-    let epan_include =
-        env::var("WIRESHARK_EPAN_HEADERS").unwrap_or("/usr/include/wireshark/epan".to_owned());
-
-    // if the wireshark updates, this script will rerun in order to generate fresh bindings
-    // backward compatibility is the responsibility of the wireshark team
-    println!("cargo:rerun-if-changed={}", epan_include);
-
-    // use pkg-config to determine where are glib headers
+    // use pkg-config to determine where are wireshark and glib headers
     let output = Command::new("pkg-config")
-        .args(&["--cflags", "glib-2.0"])
+        .args(&["--cflags", "wireshark"])
         .output()
         .unwrap();
-    let glib_includes = str::from_utf8(output.stdout.as_slice())
+    let includes = str::from_utf8(output.stdout.as_slice())
         .unwrap()
         .trim_end_matches('\n');
 
+    let base = includes.split(' ')
+        .find(|s| s.starts_with("-I") && s.ends_with("include/wireshark"))
+        .expect("wireshark installed and accessible via pkg-config")
+        .trim_start_matches("-I");
+    println!("base: {}", base);
+
+    // if the wireshark updates, this script will rerun in order to generate fresh bindings
+    // backward compatibility is the responsibility of the wireshark team
+    println!("cargo:rerun-if-changed={}", base);
+
     // configure the builder
     let bindings = bindgen::Builder::default()
-        .header(format!("{}/ftypes/ftypes.h", epan_include))
-        .header(format!("{}/proto.h", epan_include))
-        .header(format!("{}/packet.h", epan_include))
-        .header(format!("{}/conversation.h", epan_include))
-        .header(format!("{}/tvbuff.h", epan_include))
-        .header(format!("{}/tvbuff-int.h", epan_include))
-        .header(format!("{}/dissectors/packet-tcp.h", epan_include))
-        .header(format!("{}/wmem/wmem_user_cb.h", epan_include))
-        .header(format!("{}/prefs.h", epan_include))
-        .clang_args(&["-I/usr/include/wireshark", "-DHAVE_PLUGINS"])
-        .clang_args(glib_includes.split(' '))
+        .clang_args(includes.split(' '))
+        .clang_arg("-DHAVE_PLUGINS")
+        .header(format!("{}/epan/ftypes/ftypes.h", base))
+        .header(format!("{}/epan/proto.h", base))
+        .header(format!("{}/epan/packet.h", base))
+        .header(format!("{}/epan/conversation.h", base))
+        .header(format!("{}/epan/tvbuff.h", base))
+        .header(format!("{}/epan/tvbuff-int.h", base))
+        .header(format!("{}/epan/dissectors/packet-tcp.h", base))
+        .header(format!("{}/epan/wmem/wmem_user_cb.h", base))
+        .header(format!("{}/epan/prefs.h", base))
         .generate()
         .expect("Unable to generate bindings");
 
