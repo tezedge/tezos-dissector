334e4ccfa7aea35775a17df6749dac2e3e54cfbd
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-08-29 21:35:08 +0300

    improve script

--- prebuilt/install-prebuilt.sh
+++ prebuilt/install-prebuilt.sh
@@ -4,10 +4,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     mkdir -p ~/.local/lib/wireshark/plugins/3.2/epan/ && \
     pushd ~/.local/lib/wireshark/plugins/3.2/epan/ && \
     wget https://raw.githubusercontent.com/vlad9486/tezos-dissector/master/prebuilt/tezos_dissector_ubuntu_19_04.so; \
+    mv tezos_dissector_ubuntu_19_04.so tezos_dissector.so; \
     popd
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     pushd /Applications/Wireshark.app/Contents/PlugIns/wireshark/3-2/epan/ && \
-    wget https://raw.githubusercontent.com/vlad9486/tezos-dissector/master/prebuilt/tezos_dissector_macos.dylib; \
+    wget https://raw.githubusercontent.com/vlad9486/tezos-dissector/master/prebuilt/tezos_dissector_macos.dylib && \
+    mv tezos_dissector_macos.dylib tezos_dissector.so; \
     popd
 else
     echo 'OS not supported'
