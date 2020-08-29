37bd8594b23f613a09bf541b5cf3904e8a729c59
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-08-29 21:46:22 +0300

    hotfix

--- prebuilt/install-prebuilt.sh
+++ prebuilt/install-prebuilt.sh
@@ -4,7 +4,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     mkdir -p "~/.local/lib/wireshark/plugins/3.2/epan/" && \
     wget --https-only --secure-protocol=TLSv1_2 \
         "https://raw.githubusercontent.com/vlad9486/tezos-dissector/master/prebuilt/tezos_dissector_ubuntu_19_04.so" \
-        -O "~/.local/lib/wireshark/plugins/3.2/epan/tezos_dissector.so"
+        -O "$(HOME)/.local/lib/wireshark/plugins/3.2/epan/tezos_dissector.so"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     curl --proto '=https' --tlsv1.2 --silent --show-error --fail \
         --location "https://raw.githubusercontent.com/vlad9486/tezos-dissector/master/prebuilt/tezos_dissector_macos.dylib" \
