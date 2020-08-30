e9d5d71655cc26a3d87e292a78f3524aefbcdae9
Vladislav Melnik <vladislav.melnik@simplestaking.com>
2020-08-30 21:37:19 +0300

    try to build in docker

--- prebuilt/src/main.rs
+++ prebuilt/src/main.rs
@@ -1,5 +1,32 @@
 use os_type::{current_platform, OSType};
-use std::{process::Command, str::from_utf8, env, path::PathBuf, fs, io};
+use std::{process::Command, str::from_utf8, env, path::{PathBuf, Path}, fs, io};
+
+fn build_in_docker<T, P>(tag: T, output: &P)
+where
+    T: AsRef<str>,
+    P: AsRef<Path>,
+{
+    Command::new("docker")
+        .args(&["build", "-t"])
+        .arg(format!("wireshark-plugin-builder:{}", tag.as_ref()))
+        .arg("-f")
+        .arg(format!("prebuilt/wpb.{}.dockerfile", tag.as_ref()))
+        .arg(".")
+        .output()
+        .unwrap();
+    let c = Command::new("docker")
+        .arg("create")
+        .arg(format!("wireshark-plugin-builder:{}", tag.as_ref()))
+        .output()
+        .unwrap();
+    let cid = from_utf8(c.stdout.as_ref()).unwrap();
+    Command::new("docker")
+        .arg("cp")
+        .arg(format!("{}:/usr/local/tezos-dissector/target/release/libtezos-dissector.so", cid))
+        .arg(output.as_ref())
+        .output()
+        .unwrap();
+}
 
 fn get_wireshark_version() -> (u32, u32) {
     let output = Command::new("wireshark")
@@ -22,8 +49,6 @@ fn main() {
     let platform = current_platform();
     match platform.os_type {
         OSType::Ubuntu | OSType::Manjaro => {
-            let path = format!("prebuilt/libtezos_dissector_linux_{}_{}.so", major, minor);
-            let path = path.parse::<PathBuf>().unwrap();
             let plugin_path = format!(
                 "{}/.local/lib/wireshark/plugins/{}.{}/epan/",
                 env::var("HOME").unwrap(),
@@ -32,6 +57,9 @@ fn main() {
             );
             let plugin_path = plugin_path.parse::<PathBuf>().unwrap();
             fs::create_dir_all(plugin_path.clone()).unwrap();
+
+            /*let path = format!("prebuilt/libtezos_dissector_linux_{}_{}.so", major, minor);
+            let path = path.parse::<PathBuf>().unwrap();
             fs::copy(path, plugin_path.join("libtezos_dissector.so"))
                 .unwrap_or_else(|e| match e.kind() {
                     io::ErrorKind::NotFound => panic!(
@@ -40,7 +68,8 @@ fn main() {
                         minor,
                     ),
                     _ => panic!(e),
-                });
+                });*/
+            build_in_docker(platform.version, &plugin_path);
         },
         OSType::OSX => {
             let path = format!("prebuilt/libtezos_dissector_macos_{}_{}.dylib", major, minor);
